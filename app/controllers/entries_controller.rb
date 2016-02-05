f684e980ac2cf0aafaa24cc48e0321f3aff84b99
ESosa0 <ericasosa0@gmail.com>
2016-02-05 18:22:20 +0100

    added method to calculate hours in entries.rb and added total hours to index#entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,8 @@
 class EntriesController < ApplicationController
   def index
-    @project = Project.find params[:id]
+    @project = Project.find params[:project_id]
     @entries = @project.entries
+    @total = Entry.calculate_hours(@project)
   rescue ActiveRecord::RecordNotFound 
     render 'no_entries_found'
   end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,13 @@
 class Entry < ActiveRecord::Base
   belongs_to :project
+   def self.calculate_hours(id)
+    @project = Project.find(id)
+   
+    @entries = @project.entries.where("created_at > ?", Date.today.midnight - 1.month)
+    total = 0
+    @entries.each do |entry|
+      total += entry.hours*60 + entry.minutes
+    end
+    @total = (total/60.0).round(2)
+  end
 end

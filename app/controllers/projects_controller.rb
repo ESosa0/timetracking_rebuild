95b8e089a967b262c7cc5e9be821c3f20d1d8132
ESosa0 <ericasosa0@gmail.com>
2016-02-13 18:27:58 +0100

    created links and create new project feature

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,4 +9,22 @@ class ProjectsController < ApplicationController
   rescue ActiveRecord::RecordNotFound 
     render 'no_projects_found'
   end
+  def new
+    @project= Project.new
+  end
+  def create
+    @project = Project.new project_params
+    if @project.save
+      redirect_to action: 'index', controller: 'projects'
+    else
+      @errors = @project.errors.full_messages
+      render "new"
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => 'site#home'
+  root 'site#home'
   get '/contact' => 'site#contact'
   # get '/projects' => 'projects#index'
   # get '/projects/:id' => 'projects#show'
